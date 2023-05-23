613834e85064299b465a473b5320703f01b83ea2
David Profitt <dlprofitt@gmail.com>
2023-05-23 17:13:26 -0400

    MagicMoose now asks for a question and provides an answer that is pulled by index value from a list based off of a randomly generated number

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+
+    void MooseSays(string message)
+    {
+        Console.WriteLine(@"
+   *         *   *         *
+         *           ()           *
+    *   ___      ___// ____   * 
+ *     {   \   _/ * / /    }    *   *
+  *    {__  \_/ *  /_/  __/      *
+    *    _\  /_*__*_\   /__     *
+        {___(_______/   ___}  *    *
+ *    *     \_  ~~ ~~  /            *
+         *    |(o} (o}  \_      * *
+   *          | ^   ^         *
+    ,   *   _/          __       *
+  __|\__  (o)  (o) {   }     *
+   \   /  |_      _|\_/             *
+ *  |/\|  [________],        *     *
+    ` \\     \\___//       *
+       \\     ^^^^^              *
+");
+
+        Console.WriteLine(message);
+
+    }
+
+    void MooseSpeaks(string message)
+{
+    Console.WriteLine(message);
+}
+
+string Answer(int num)
+{
+    List<string> answers = new List<string> {
+        "As I see it, yes.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "It is certain.",
+        "It is decidedly so.",
+        "Most likely.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Outlook good.",
+        "Reply hazy, try again.",
+        "Signs point to yes.",
+        "Very doubtful.",
+        "Without a doubt.",
+        "Yes.",
+        "Yes - definitely.",
+        "You may rely on it."
+    };
+
+    return answers[num];
+}
+
+    MooseSays("*** Hello, 'tis I, the Magic Moose! ***");
+    MooseSpeaks("Ask any question, and I shall provide an answer.");
+
+int Random()
+{
+    Random r = new Random();
+    int genRand = r.Next(0, 19);
+    return genRand;
+}
+
+bool MooseAsks(string question)
+{
+    Console.Write($"{question}");
+    string answer = Console.ReadLine();
+    if (answer == null || answer == "")
+    {
+        return false;
+    }
+    else if (answer != "")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void MooseReplies()
+{
+    MooseAsks("What is thine question?");
+    MooseSpeaks(Answer(Random()));
+}
+
+MooseReplies();
